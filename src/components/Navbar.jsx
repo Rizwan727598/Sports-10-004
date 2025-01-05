1a105332ca249e7a0beb1125fb4cdf792c41141c
Rizwan727598 <rizwanislam369@gmail.com>
2025-01-06 00:05:40 +0600

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,29 @@ const Navbar = () => {
             backgroundColor: darkMode ? "#222" : "#fff",
             borderRadius: "8px",
           }}
-        ></button>
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke={darkMode ? "#fff" : "#000"}
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            {menuOpen ? (
+              <line x1="18" y1="6" x2="6" y2="18" />
+            ) : (
+              <>
+                <line x1="3" y1="12" x2="21" y2="12" />
+                <line x1="3" y1="6" x2="21" y2="6" />
+                <line x1="3" y1="18" x2="21" y2="18" />
+              </>
+            )}
+          </svg>
+        </button>
 
         {/* Navigation Links */}
         <ul
